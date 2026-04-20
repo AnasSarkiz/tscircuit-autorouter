4b869d5917a4fa16d201d3392f4c8e6f5ef9ada5
AnasSarkiz <anassoftwaredev@gmail.com>
2026-04-20 17:14:45 +0200

    formated

--- fixtures/bug-reports/bugreport50-e1c37669/bugreport50-e1c37669.fixture.tsx
+++ fixtures/bug-reports/bugreport50-e1c37669/bugreport50-e1c37669.fixture.tsx
@@ -3,9 +3,7 @@ import { AutoroutingPipelineDebugger } from "lib/testing/AutoroutingPipelineDebu
 import bugReportJson from "./bugreport50-e1c37669.json"
 export default () => {
   const srj =
-  (bugReportJson as any).simple_route_json ??
-  (bugReportJson as any).autorouting_bug_report?.simple_route_json
-  return (
-    <AutoroutingPipelineDebugger srj={srj} />
-  )
+    (bugReportJson as any).simple_route_json ??
+    (bugReportJson as any).autorouting_bug_report?.simple_route_json
+  return <AutoroutingPipelineDebugger srj={srj} />
 }
